7affab4f811a67acd8a84999657f5485b944e2bd
Irene San Roman <irenesrfuentes@gmail.com>
2024-10-24 12:02:50 +0200

    Fin Sección 7

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,7 +10,10 @@ export class GifsService {
   private _tagsHistory: string[] = [];
   private apiKey: string = 'wqv79Hf7m817MMfDGiNXhDAFEfN6RFzy';
   private serviceUrl : string = 'https://api.giphy.com/v1/gifs'
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+    console.log('Gifs Service Ready')
+   }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -24,6 +27,19 @@ export class GifsService {
 
     this._tagsHistory.unshift( tag );
     this._tagsHistory = this.tagsHistory.splice(0,10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void{
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+  private loadLocalStorage(): void{
+    if (!localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if (this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
+
   }
 
  searchTag( tag: string ): void {
